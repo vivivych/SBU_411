9ea607518d9c4380bff40e24081d556f5899698d
User <User@DESKTOP-0TCDSR6>
2025-02-08 14:35:05 +0300

    Geometry in progress

--- ControlStructures/Geometry/main.cpp
+++ ControlStructures/Geometry/main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 //#define SQUARE
-#define TRIANGLE_1
-#define TRIANGLE_2
+//#define TRIANGLE_1
+//#define TRIANGLE_2
 #define TRIANGLE_3
 #define TRIANGLE_4
 
@@ -26,6 +26,33 @@ void main()
 	cout << endl;
 #endif // SQUARE
 
+#ifdef TRIANGLE_1
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+#endif // TRIANGLE_1
 
+#ifdef TRIANGLE_2
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+#endif // TRIANGLE_2
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)cout << "  ";
+		for (int j = i; j < n; j++)cout << "* ";
+		cout << endl;
+	}
 
 }
