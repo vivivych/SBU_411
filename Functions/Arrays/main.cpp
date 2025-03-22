2e20d739e5618df2bba9b5ec95483b18b398e8d0
User <User@DESKTOP-0TCDSR6>
2025-03-22 13:29:19 +0300

    Functions day 1

---
+++ Functions/Arrays/main.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);	//Заполняет массив случайными числами
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+
+	FillRand(arr, n);	//Заполнение массива случайными числами:
+	Print(arr, n);		//Вывод исходного массива на экран:
+	Sort(arr, n);		//Сортировка массива:
+	Print(arr, n);		//Вывод отсортированного массива на экран:
+
+	const int SIZE = 8;
+	int brr[SIZE];
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	Sort(brr, SIZE);
+	Print(brr, SIZE);
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100; //функция возвращает псевдослучайное число в диапозоне от 0 до 32676
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
---
+++ Functions/Functions/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+using namespace std;
+
+int Sum(int a = 0, int b = 0, int c = 0);	//Прототип функции (Объявление функции - Function declaration)
+int Difference(int a, int b);
+int Product(int a, int b);
+double Quotient(int a, int b);
+//C2660: Function doesn't take N arguments
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello Functions" << endl;
+	int a = 2;
+	int b = 3;
+	int c = Sum(a, b, 11);		//Использование функции (Вызов функции - Function call)
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Difference(a, b) << endl;
+	cout << a << " * " << b << " = " << Product(a, b) << endl;
+	cout << a << " / " << b << " = " << Quotient(a, b) << endl;
+	cout << 4 << " + " << 5 << " = " << Sum(4, 5) << endl;
+	cout << Sum() << endl;
+}
+
+int Sum(int a, int b, int c)		//Реализация функции (Определение функции - Function definition)
+{
+	int res = a + b + c;
+	return res;
+}
+int Difference(int a, int b)
+{
+	//Difference - Разность
+	return a - b;
+}
+int Product(int a, int b)
+{
+	//Product - произведение
+	return a * b;
+}
+double Quotient(int a, int b)
+{
+	//Quotient - частное
+	return (double)a / b;
+}
