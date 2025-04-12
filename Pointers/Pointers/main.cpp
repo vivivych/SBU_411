1fcf6488005d2e8298450de6516cd5b6cf722734
User <User@DESKTOP-0TCDSR6>
2025-04-12 13:09:09 +0300

    Transfer parameters start

---
+++ Pointers/Pointers/main.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;	//вывод переменной 'a' на экран
+	cout << &a << endl;	//взятие адреса переменной 'a' прямо при выводе
+	cout << pa << endl;	//вывод адреса переменной 'a', хранящегося в указателе 'pa'
+	cout << *pa << endl;
+}
+/*
+---------------------------------
+	& - Оператор взятия адреса (Address-of operator)
+	* - Оператор разыменования (Dereference operator)
+---------------------------------
+*/
