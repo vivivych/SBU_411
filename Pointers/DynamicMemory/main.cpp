98a02170c030218e83e7e5f2d5afbb8d33097439
User <User@DESKTOP-QHG18FL>
2025-04-26 13:42:00 +0300

    DynamicMemory day 1

---
+++ Pointers/DynamicMemory/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "¬ведите размер массива: "; cin >> n;
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+	delete[] arr;
+	//Memory leak
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+		//Index operator, Subscript operator;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
