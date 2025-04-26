c8421c11ae04357373ae6e387e1df1532837f788
User <User@DESKTOP-QHG18FL>
2025-04-26 11:45:27 +0300

    Templation DONE

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 
 void FillRand(int arr[], const int n);	//Заполняет массив случайными числами
-void Print(int arr[], const int n);
-void Sort(int arr[], const int n);
-int Sum(int arr[], const int n);
-double Avg(int arr[], const int n);
-int MinValueIn(int arr[], const int n);
-int MaxValueIn(int arr[], const int n);
-void ShiftLeft(int arr[], const int n, const int shifts);
+void FillRand(char arr[], const int n);	//Заполняет массив случайными числами
+void FillRand(float arr[], const int n);	//Заполняет массив случайными числами
+void FillRand(double arr[], const int n);	//Заполняет массив случайными числами
+
+template<typename T>void Print(T arr[], const int n);
+template<typename T>void Sort(T arr[], const int n);
+template<typename T>T Sum(T arr[], const int n);
+template<typename T>double Avg(T arr[], const int n);
+template<typename T>T MinValueIn(T arr[], const int n);
+template<typename T>T MaxValueIn(T arr[], const int n);
+template<typename T>void ShiftLeft(T arr[], const int n, const int shifts);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 5;
-	int arr[n];
+	float arr[n];
 
 	FillRand(arr, n);	//Заполнение массива случайными числами:
 	Print(arr, n);		//Вывод исходного массива на экран:
@@ -26,7 +30,7 @@ void main()
 	cout << "Максимальное значение в массиве: " << MaxValueIn(arr, n) << endl;
 
 	const int SIZE = 8;
-	int brr[SIZE];
+	char brr[SIZE];
 	FillRand(brr, SIZE);
 	Print(brr, SIZE);
 	Sort(brr, SIZE);
@@ -36,11 +40,11 @@ void main()
 	cout << "Минимальное значение в массиве: " << MinValueIn(brr, SIZE) << endl;
 	cout << "Максимальное значение в массиве: " << MaxValueIn(brr, SIZE) << endl;
 
-	Print(brr, SIZE);
+	/*Print(brr, SIZE);
 	int shifts;
 	cout << "Введите количество сдвигов: "; cin >> shifts;
 	ShiftLeft(brr, SIZE, shifts);
-	Print(brr, SIZE);
+	Print(brr, SIZE);*/
 }
 
 void FillRand(int arr[], const int n)
@@ -50,7 +54,33 @@ void FillRand(int arr[], const int n)
 		arr[i] = rand() % 100; //функция возвращает псевдослучайное число в диапозоне от 0 до 32676
 	}
 }
-void Print(int arr[], const int n)
+void FillRand(char arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand(); //функция возвращает псевдослучайное число в диапозоне от 0 до 32676
+	}
+}
+
+void FillRand(float arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000; //функция возвращает псевдослучайное число в диапозоне от 0 до 32676
+		arr[i] /= 100;
+	}
+}
+void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000; //функция возвращает псевдослучайное число в диапозоне от 0 до 32676
+		arr[i] /= 100;
+	}
+}
+
+
+template<typename T>void Print(T arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -58,7 +88,7 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
-void Sort(int arr[], const int n)
+template<typename T>void Sort(T arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -66,49 +96,49 @@ void Sort(int arr[], const int n)
 		{
 			if (arr[j] < arr[i])
 			{
-				int buffer = arr[i];
+				T buffer = arr[i];
 				arr[i] = arr[j];
 				arr[j] = buffer;
 			}
 		}
 	}
 }
-int Sum(int arr[], const int n)
+template<typename T>T Sum(T arr[], const int n)
 {
-	int sum = 0;
+	T sum = 0;
 	for (int i = 0; i < n; i++)
 	{
 		sum += arr[i];
 	}
 	return sum;
 }
-double Avg(int arr[], const int n)
+template<typename T>double Avg(T arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
 }
-int MinValueIn(int arr[], const int n)
+template<typename T>T MinValueIn(T arr[], const int n)
 {
-	int min = arr[0];
+	T min = arr[0];
 	for (int i = 1; i < n; i++)
 	{
 		if (arr[i] < min)min = arr[i];
 	}
 	return min;
 }
-int MaxValueIn(int arr[], const int n)
+template<typename T>T MaxValueIn(T arr[], const int n)
 {
-	int max = arr[0];
+	T max = arr[0];
 	for (int i = 1; i < n; i++)
 	{
 		if (arr[i] > max)max = arr[i];
 	}
 	return max;
 }
-void ShiftLeft(int arr[], const int n, const int shifts)
+template<typename T>void ShiftLeft(T arr[], const int n, const int shifts)
 {
 	for (int i = 0; i < shifts; i++)
 	{
-		int buffer = arr[0];
+		T buffer = arr[0];
 		for (int i = 1; i < n; i++)
 		{
 			arr[i - 1] = arr[i];
